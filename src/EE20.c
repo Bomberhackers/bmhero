5c94d1b673725f4396d0610eb8c4fc24f4d28174
Coockie1173 <rudjerud@hotmail.com>
2023-09-12 23:27:34 +0200

    decomp EE20 (memset, memcpy, ?)

---
+++ src/EE20.c
@@ -0,0 +1,58 @@
+#include <ultra64.h>
+
+void func_80017FD8(s32*);                                /* extern */
+extern s32* D_8004A370;
+extern s32 D_8004BBB0;
+extern s32 D_80380000;
+
+// memset?
+void* func_8000E220(void* dest, u32 c, s32 size) 
+{
+    u8 *dest_new = (u8*)dest;
+    
+    while(size --) {
+        *(dest_new++) = c;
+    }
+
+    return dest;
+}
+
+// memcpy?
+void* func_8000E274(void* dest, u32* src, s32 size)
+{
+    u32 Unk00 = (u32)dest; //C
+    u32 Unk01 = (u32)src; //8
+    
+    if(size != 0 && (dest != src))
+    {
+        if(Unk00 > Unk01 && Unk00 < ((u32)Unk01 + size))
+        {
+            while(size --)
+            {
+                *(u8*)(size + (u32)Unk00) = *(u8*)(size + (u32)Unk01);
+            }
+        }
+        else
+        {
+            while(size --)
+            {
+                *(u8*)(Unk00) = *(u8*)Unk01, Unk00 += 1, Unk01 += 1;
+            }
+        }
+    }
+
+    return dest;
+}
+
+s32* func_8000E34C(u32 arg0) {
+    s32* Unk00;
+    Unk00 = D_8004A370;
+    D_8004A370 = arg0 + (s32)D_8004A370;
+
+    if (((s32)D_8004A370 - (s32)&D_80380000) >= 0x80000) {
+        func_80017FD8(&D_8004BBB0);
+        return -1;
+    }
+
+    return Unk00;
+}

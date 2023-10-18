2342080ed31a6c20ed7a70ee158685e3c1691d00
RevoSucks <projectrevotpp@hotmail.com>
2023-10-17 20:08:39 -0400

    func_8000EEE8

--- src/boot/F280.c
+++ src/boot/F280.c
@@ -16,7 +16,9 @@ struct UnkInputStruct8000E680 {
 };
 
 struct UnkStruct8016E104 {
-    Mtx *unk0[4][0x10];
+    char filler0[0xC0];
+    /* 0xC0 */ Hilite hilites[2];
+    /* 0xE0 */ Mtx unkE0[1];
 };
 
 struct UnkInnerStruct8000E944 {
@@ -31,20 +33,45 @@ struct UnkInnerStruct8000E944 {
     u32 unk2C;
 };
 
+struct UnkInputStruct8000EEE8_SPE4 {
+    u8 unk0;
+    u8 unk1;
+    u8 unk2;
+    u8 unk3;
+};
+
+struct UnkInputStruct8000EEE8_SPEC {
+    s32 unk0; // type of unk4 union?
+    union {
+        f32 unk4_as_f;
+        s32 unk4_as_s;
+    } u;
+    Gfx *dlist;
+};
+
 // externs
-s32 func_8000EEE8(void**, s32, s32, s32, s32, s32, s32); /* extern */
 s32 func_80013AE0(f32*, f32, f32, f32);               /* extern */
 s32 func_80013B70(f32*, f32, f32, f32);               /* extern */
 s32 func_80013F6C(f32*, f32);                         /* extern */
 s32 func_80014098(f32*, f32);                         /* extern */
 s32 func_800141C4(f32*, f32);                         /* extern */
 
+extern u8 D_1000768[];
+extern s32 D_8004A390;
+extern s32 D_8004A394;
+extern f32 D_800557E0[4];
 extern s32 D_80055820;
 extern s32 D_80055828;
 extern u32 D_80055D30[];
 extern u32 D_80055D40[];
+extern f32 D_8016E3B4;
+extern f32 D_8016E3BC;
+extern f32 D_8016E3C4;
 
 extern struct UnkStruct8016E104 *D_8016E104;
+
+// functions
+s32 func_8000EEE8(Gfx** gfx, s32 arg1, s32 arg2, s32 arg3, s32 arg4, s32 arg5, s32 arg6);
 
 void func_8000E680(struct UnkInputStruct8000E680* arg0, struct UnkInputStruct8000E680* arg1) {
     f32 sp3C;
@@ -145,9 +172,9 @@ s32 func_8000E944(Gfx** gfx, s32 arg1, s32 arg2, s32 arg3, s32 arg4, s32 arg5, s
         D_80055820 += 1;
         guMtxCatF(mf, (D_80055820 << 6) - 0x40 + (u32)&D_80055828, (D_80055820 << 6) + (u32)&D_80055828);
     }
-    guMtxF2L((D_80055820 << 6) + (u32)&D_80055828, (void*)((u32)&D_8016E104->unk0[arg7] + 0xE0));
+    guMtxF2L((D_80055820 << 6) + (u32)&D_80055828, &D_8016E104->unkE0[arg7]);
 
-    gSPMatrix(dlist++, osVirtualToPhysical((void*)((u32)&D_8016E104->unk0[arg7++] + 0xE0)), G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
+    gSPMatrix(dlist++, osVirtualToPhysical(&D_8016E104->unkE0[arg7++]), G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
     
     if (sp84[arg6].u.unk0 >= 0) {
         arg7 = func_8000EEE8(&dlist, arg1, arg2, arg3, arg4, sp84[arg6].u.unk0, arg7);
@@ -166,7 +193,88 @@ s32 func_8000E944(Gfx** gfx, s32 arg1, s32 arg2, s32 arg3, s32 arg4, s32 arg5, s
     return arg7;
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/boot/F280/func_8000EEE8.s")
+s32 func_8000EEE8(Gfx** gfx, s32 arg1, s32 arg2, s32 arg3, s32 arg4, s32 arg5, s32 arg6) {
+    struct UnkInputStruct8000EEE8_SPEC *spEC;
+    Gfx* dlist;
+    struct UnkInputStruct8000EEE8_SPE4* spE4;
+    f32 spA4[4][4];
+    s32 spA0;
+    
+
+    spA0 = 0;
+    spE4 = arg1 + 8;
+    spEC = (void*)(arg1 + 0xC);
+    dlist = *gfx;
+    guMtxIdentF(spA4);
+
+    gSPSegment(dlist++, spE4->unk0, (s32)(arg1 + 0x80000000));
+    gSPSegment(dlist++, spE4->unk1, (s32)(arg2 + 0x80000000));
+    gSPSegment(dlist++, spE4->unk2, (s32)(arg3 + 0x80000000));
+    gSPSegment(dlist++, spE4->unk3, (s32)(arg4 + 0x80000000));
+    gDPPipelineMode(dlist++, G_PM_1PRIMITIVE);
+
+    if ((spEC[arg5].unk0 == 0) 
+     || (spEC[arg5].unk0 == 5) 
+     || (spEC[arg5].unk0 == 6)
+     || (spEC[arg5].unk0 == 8)) {
+        switch (spEC[arg5].unk0) {                          /* irregular */
+        case 5:
+            guMtxIdentF((f32 (*)[4]) D_800557E0);
+            func_8000E680(D_800557E0, (D_80055820 << 6) + (u32)&D_80055828);
+            func_80013B70(D_800557E0, D_8016E3B4, D_8016E3BC, D_8016E3C4);
+            D_80055820 += 1;
+            guMtxCatF((f32 (*)[4]) D_800557E0, (D_80055820 << 6) - 0x40 + (u32)&D_80055828, (D_80055820 << 6) + (u32)&D_80055828);
+            spA0 += 1;
+            break;
+        case 6:
+            D_8004A390 = 1;
+            gDPSetTextureLUT(dlist++, G_TT_NONE);
+            gSPSetGeometryMode(dlist++, G_TEXTURE_GEN);
+            gSPTexture(dlist++, 0x07C0, 0x07C0, 0, G_TX_RENDERTILE, G_ON);
+            gDPSetTexturePersp(dlist++, G_TP_PERSP);
+            gDPSetPrimColor(dlist++, 0, 0, 255, 255, 255, 255);
+            gDPSetEnvColor(dlist++, 64, 64, 64, 255);
+            gDPSetCombineMode(dlist++, G_CC_HILITERGBA, G_CC_HILITERGBA);
+            gDPSetTextureImage(dlist++, G_IM_FMT_IA, G_IM_SIZ_16b, 1, D_1000768);
+            gDPSetTile(dlist++, G_IM_FMT_IA, G_IM_SIZ_16b, 0, 0x0000, G_TX_LOADTILE, 0, G_TX_NOMIRROR | G_TX_WRAP, 5, G_TX_NOLOD, G_TX_NOMIRROR | G_TX_WRAP, 5, G_TX_NOLOD);
+            gDPLoadSync(dlist++);
+            gDPLoadBlock(dlist++, G_TX_LOADTILE, 0, 0, 511, 512);
+            gDPPipeSync(dlist++);
+            gDPSetTile(dlist++, G_IM_FMT_IA, G_IM_SIZ_8b, 4, 0x0000, G_TX_RENDERTILE, 0, G_TX_NOMIRROR | G_TX_WRAP, 5, G_TX_NOLOD, G_TX_NOMIRROR | G_TX_WRAP, 5, G_TX_NOLOD);
+            gDPSetTileSize(dlist++, G_TX_RENDERTILE, 0, 0, 0x007C, 0x007C);
+            gDPSetHilite1Tile(dlist++, G_TX_RENDERTILE, &D_8016E104->hilites[0], 32, 32);
+            break;
+        case 8:
+            D_8004A394 = 1;
+            gSPSetGeometryMode(dlist++, G_TEXTURE_GEN);
+            break;
+        default:
+            if (D_8004A390 != 0) {
+                gSPClearGeometryMode(dlist++, G_TEXTURE_GEN);
+                gSPTexture(dlist++, 0, 0, 0, G_TX_RENDERTILE, G_OFF);
+                D_8004A390 = 0;
+            } else if (D_8004A394 != 0) {
+                gSPClearGeometryMode(dlist++, G_TEXTURE_GEN);
+                D_8004A394 = 0;
+            }
+            break;
+        }
+        if (spEC[arg5].u.unk4_as_f != 1.0f) {
+            func_80013B70(&spA4[0], spEC[arg5].u.unk4_as_f, spEC[arg5].u.unk4_as_f, spEC[arg5].u.unk4_as_f);
+            D_80055820 += 1;
+            guMtxCatF((f32 (*)[4]) &spA4[0], (D_80055820 << 6) - 0x40 + (u32)&D_80055828, (D_80055820 << 6) + (u32)&D_80055828);
+            spA0 += 1;
+        }
+        guMtxF2L((D_80055820 << 6) + (u32)&D_80055828, &D_8016E104->unkE0[arg6]);
+        gSPMatrix(dlist++, osVirtualToPhysical(&D_8016E104->unkE0[arg6++]), G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
+        gSPDisplayList(dlist++, (s32)(spEC[arg5].dlist) + arg1);
+        D_80055820 -= spA0;
+    } else if (spEC[arg5].unk0 == 1) {
+        arg6 = func_8000E944(&dlist, arg1, arg2, arg3, arg4, spEC[arg5].dlist, spEC[arg5].u.unk4_as_s, arg6);
+    }
+    *gfx = dlist;
+    return arg6;
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/boot/F280/func_8000F888.s")
 

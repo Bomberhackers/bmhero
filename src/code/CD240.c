ee45b1973c1b9aa64e444442009176697094624c
BeckSM64 <BeckSM64@gmail.com>
2023-10-21 12:54:20 -0400

    func_800DB504

--- src/code/CD240.c
+++ src/code/CD240.c
@@ -179,7 +179,9 @@ void func_800DB3E8(void) {
     }
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/code/CD240/func_800DB504.s")
+void func_800DB504(void) {
+    func_8002B0E4(D_80177A60);
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/code/CD240/func_800DB534.s")
 

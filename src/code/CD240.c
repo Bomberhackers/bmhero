f2ae59684aba2785f082421771e78ea92fc220c7
BeckSM64 <BeckSM64@gmail.com>
2024-07-08 16:06:04 -0400

    decomp func_800DF3D4

--- src/code/CD240.c
+++ src/code/CD240.c
@@ -1137,7 +1137,54 @@ void func_800DF034(void) {
     }
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/code/CD240/func_800DF3D4.s")
+void func_800DF3D4(void) {
+    struct PlayerStruct* sp44;
+    f32 sp40;
+    f32 temp_f20;
+
+    sp44 = &gPlayerData[D_80177A60];
+    if (sp44->unk132 == 0) {
+        sp44->unk132 = (u8) (sp44->unk132 + 1);
+        func_8001C0EC(D_80177A60, 0, 0xB, 0x70, &D_80118FF4);
+        func_8001ABF4(D_80177A60, 0, 0, &D_80119194);
+        func_8001ABF4(D_80177A60, 1, 0, &D_80119194.Rot.y);
+        func_80029EF8(D_80177A60, -5.0f, 0.0f);
+        sp44->unk44 = 6.0f;
+        sp44->unkA6 = 0;
+        sp44->unkB2 = 2;
+    }
+    if (func_80028FA0(D_80177A60) != 0) {
+        if ((sp44->unk106 == 0xA1) || (sp44->unk106 == 0xA2)) {
+            func_800E3EE4();
+            return;
+        } else {
+            func_80029B60(D_80177A60);
+        }
+    }
+
+    func_8002A8B4(D_80177A60, 5.0f);
+    sp44->Rot.y = (f32) sp44->unk3C;
+    sp40 = sp44->Vel.y;
+    sp44->Vel.y = 0.0f;
+    func_80029C40(D_80177A60);
+    if (func_80029018(D_80177A60, 0, 80.0f, 0, 0.0f, 0.0f) != 0) {
+        func_80029824(D_80177A60, func_800297DC());
+    }
+    sp44->Vel.y = sp40;
+    if (func_80029F58(D_80177A60, 0, 0, 0, 30.0f) == 1) {
+        sp44->Vel.z = 0.0f;
+        sp44->Vel.x = sp44->Vel.y = sp44->Vel.z;
+        sp44->unk44 = 0.0f;
+    }
+    if (func_8001B4AC(D_80177A60, 0) != 0) {
+        sp44->Vel.z = 0.0f;
+        sp44->Vel.x = sp44->Vel.y = sp44->Vel.z;
+        sp44->unkA4 = 3;
+        sp44->unkA6 = 0;
+        sp44->unkB2 = 0;
+        sp44->unk132 = 0U;
+    }
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/code/CD240/func_800DF704.s")
 

bee05ceac5648a45eb1dd7d2ad75774489275063
BeckSM64 <BeckSM64@gmail.com>
2024-07-08 14:41:18 -0400

    decomp func_800DEB3C

--- src/code/CD240.c
+++ src/code/CD240.c
@@ -1003,7 +1003,30 @@ void func_800DE95C(void) {
     }
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/code/CD240/func_800DEB3C.s")
+void func_800DEB3C(void) {
+    struct PlayerStruct* sp24;
+
+    sp24 = &gPlayerData[D_80177A60];
+    if (sp24->unk132 == 0) {
+        sp24->unk132 = (u8) (sp24->unk132 + 1);
+        func_8001C0EC(D_80177A60, 0, 4, 0x70, &D_80118FF4);
+        func_8001ABF4(D_80177A60, 0, 0, &D_80119194);
+        func_8001ABF4(D_80177A60, 1, 0, &D_80119194.Rot.y);
+        sp24->unkA6 = 0;
+        sp24->unkA8 = 0;
+        sp24->Vel.y = 0.0f;
+    }
+    if ((func_80028FA0(D_80177A60) != 0) && (((sp24->unk106 == 0xA2)) || (sp24->unk106 == 0xA1))) {
+        func_800E3EE4();
+        return;
+    }
+    func_8002A8B4(D_80177A60, 6.0f);
+    sp24->Rot.y = (f32) sp24->unk3C;
+    if (func_8001B4AC(D_80177A60, 0) != 0) {
+        sp24->unkA4 = 4;
+        sp24->unk132 = 0U;
+    }
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/code/CD240/func_800DECE0.s")
 

ebfaf26e3ca22b1bc374de014582a25d762e8d5e
BeckSM64 <BeckSM64@gmail.com>
2024-07-10 01:50:30 -0400

    decomp func_800E2A00

--- src/code/CD240.c
+++ src/code/CD240.c
@@ -1955,7 +1955,9 @@ void func_800E2738(void) {
     }
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/code/CD240/func_800E2A00.s")
+void func_800E2A00(void) {
+    func_8002B0E4(D_80177A60);
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/code/CD240/func_800E2A30.s")
 

e47f55e10111c6293ede1629b5c0ea3b431d657f
LLONSIT <76184758+LLONSIT@users.noreply.github.com>
2024-07-26 22:12:00 -0500

    Documentation in src/boot/1FAB0.c (#44)

--- src/boot/1FAB0.c
+++ src/boot/1FAB0.c
@@ -1,5 +1,25 @@
+#include "prevent_bss_reordering.h"
 #include <ultra64.h>
 #include "1050.h"
+
+// File types
+typedef enum {
+    MAIN_MENU,
+    UNK_MENU
+} BACKUP_MEM_TEST_MENUS;
+
+typedef enum {
+    HAVE_RUMBLE_PAK,
+    NO_RUMBLE_PAK,
+    UNK_STATE
+} RUMBLE_PACK_STATES;
+
+typedef enum {
+    PAK_DATA_WRITE,
+    PAK_DATA_READ,
+    EEPROM_WRITE,
+    EEPROM_READ
+} BACKUP_MEM_TEST_MENU_OPTS;
 
 // File functions
 void func_8001F4B8(struct UnkStruct_8001EFD0*, s32);
@@ -11,8 +31,8 @@ void func_8001FBAC(void);
 void func_80020C44(void);
 void* func_80020F18(void);
 
-int func_8001EEB0(void) {
-    return HW_REG(SI_STATUS_REG, unsigned) & 3;
+int GetSi_DMA_Status(void) {
+     return HW_REG(SI_STATUS_REG, unsigned) & (SI_STATUS_DMA_BUSY | SI_STATUS_RD_BUSY);
 }
 
 void func_8001EED8(void) {
@@ -128,16 +148,12 @@ s32 func_8001F68C(struct UnkStruct_8001EFD0* arg0, s32 arg1, s32 arg2, s32 arg3,
 #pragma GLOBAL_ASM("asm/nonmatchings/boot/1FAB0/func_8001F68C.s")
 #endif
 
-#ifdef NON_MATCHING
 s32 func_8001F728(struct UnkStruct_8001EFD0* arg0, s32 arg1, OSPfsState* arg2) {
     s32 sp24;
 
-    sp24 = osPfsDeleteFile((OSPfs* )&arg0[arg1], arg2->company_code, arg2->game_code, (u8*)arg2->game_name, (u8*)arg2->ext_name);    
+    sp24 = osPfsDeleteFile((u32)arg0 + (arg1 * 0x84), arg2->company_code, arg2->game_code, (u8*)arg2->game_name, (u8*)arg2->ext_name);    
     return sp24;
 }
-#else
-#pragma GLOBAL_ASM("asm/nonmatchings/boot/1FAB0/func_8001F728.s")
-#endif
 
 s32 Eeprom_Write(OSMesgQueue* mq, u8* buf, u8 addr, u16 half_bytes) {
     s32 status;
@@ -150,7 +166,6 @@ s32 Eeprom_Write(OSMesgQueue* mq, u8* buf, u8 addr, u16 half_bytes) {
     status = osEepromLongWrite(mq, addr, buf, half_bytes);
     return status;
 }
-
 
 s32 Eeprom_Read(OSMesgQueue* mq, u8* buf, u8 addr, u16 half_bytes) {
     s32 status;
@@ -166,34 +181,33 @@ s32 Eeprom_Read(OSMesgQueue* mq, u8* buf, u8 addr, u16 half_bytes) {
 s32 func_8001F8A0(void) {
     s32 sp1C;
 
-    if (D_80056DF8 != 0) {
+    if (gRumblePakState != 0) {
         return 1;
     }
-    if (func_8001EEB0() != 0) {
+    if (GetSi_DMA_Status() != 0) {
         return 1;
     }
 
-    sp1C = osMotorStart(&D_80056D90);;
+    sp1C = osMotorStart(&D_80056D90);
     if (sp1C != 0) {
-        D_80056DF8 = 2; 
+        gRumblePakState = UNK_STATE; 
         osMotorInit(&gContMesgQueue, &D_80056D90, 0);
     }
 }
 
-
 s32 func_8001F938(void) {
     s32 sp1C;
 
-    if (D_80056DF8 != 0) {
+    if (gRumblePakState != HAVE_RUMBLE_PAK) {
         return 1;
     }
-    if (func_8001EEB0() != 0) {
+    if (GetSi_DMA_Status() != HAVE_RUMBLE_PAK) {
         return 1;
     }
 
-    sp1C = osMotorStop(&D_80056D90);;
+    sp1C = osMotorStop(&D_80056D90);
     if (sp1C != 0) {
-        D_80056DF8 = 2;
+        gRumblePakState = UNK_STATE;
         osMotorInit(&gContMesgQueue, &D_80056D90, 0);
         osMotorStop(&D_80056D90);
     }
@@ -203,12 +217,12 @@ void func_8001F9DC(void) {
     if (D_80056E20 != 0) {
         return;
     }
-    if (func_8001EEB0() != 0) {
-        D_80056DF8 = 1;
+    if (GetSi_DMA_Status() != 0) {
+        gRumblePakState = NO_RUMBLE_PAK;
     } else if (osMotorInit(&gContMesgQueue, &D_80056D90, 0) != 0) {
-        D_80056DF8 = 1;
+        gRumblePakState = NO_RUMBLE_PAK;
     } else {
-        D_80056DF8 = 0;
+        gRumblePakState = HAVE_RUMBLE_PAK;
     }
     D_80056DFC = 0;
     D_80056E00 = 0;
@@ -219,7 +233,7 @@ void func_8001F9DC(void) {
     D_80056E14 = 0;
     D_80056E18 = 0;
     D_80056E1C = 0;
-    if (D_80056DF8 == 0) {
+    if (gRumblePakState == HAVE_RUMBLE_PAK) {
         func_8001F938();
     }
 }
@@ -227,16 +241,16 @@ void func_8001F9DC(void) {
 void func_8001FAD4(void) {
     D_80056E20 = 0;
     func_8001F9DC();
-    if (D_80056DF8 == 0) {
+    if (gRumblePakState == HAVE_RUMBLE_PAK) {
         D_80056E20 = 0;
     } else {
         D_80056E20 = 1;
-        D_80056DF8 = 1;
+        gRumblePakState = NO_RUMBLE_PAK;
     }
 }
 
 s32 func_8001FB3C(s32 arg0, s32 arg1, s32 arg2) {
-    if (D_80056DF8 != 0) {
+    if (gRumblePakState != HAVE_RUMBLE_PAK) {
         return 1;
     }
     D_80056DFC = 4;
@@ -248,9 +262,8 @@ s32 func_8001FB3C(s32 arg0, s32 arg1, s32 arg2) {
     D_80056E14 = 0;
 }
 
-
 void func_8001FBAC(void) {
-    if (D_80056DF8 != 0) {
+    if (gRumblePakState != 0) {
         if (D_80056E1C >= 0xF) {
             D_80056E1C = 0;
             func_8001F9DC();
@@ -367,7 +380,7 @@ void func_8001FF80(void) {
 void func_800200D8(void) {
     s32 sp24;
 
-    if (gContButtonPressed[0] & 0x8000) {
+    if (gContButtonPressed[0] & CONT_A) {
         sp24 = 0;
         if (D_80056E98[sp24].unk0 == 0) {
             func_8001F174(D_80056E28, sp24);
@@ -385,15 +398,14 @@ void func_800200D8(void) {
     }
 }
 
-
 void func_8002021C(void) {
     s32 sp24;
-    s32 sp20;
+    s32 i;
 
-    if (gContButtonPressed[0] & 0x8000) {
+    if (gContButtonPressed[0] & CONT_A) {
         sp24 = 0;
-        for (sp20 = 0; sp20 < 0x20; sp20++) {
-            D_80057240[sp20] = 0;
+        for (i = 0; i < 0x20; i++) {
+            D_80057240[i] = 0;
         }
         if (D_80056E98[sp24].unk0 == 0) {
             func_8001F174(D_80056E28, sp24);
@@ -404,49 +416,49 @@ void func_8002021C(void) {
                 }
             }
         }
-        sp20 = 0;
+        i = 0;
         do {
-        } while (++(sp20) < 0x20);
+        } while (++(i) < 0x20);
     }
 }
 
 
-void func_80020360(void) {
-    if ((gContButtonPressed[0] & 0x8000) && (Eeprom_Write(&gContMesgQueue, &D_8004A610, 0, 0x200) != 0)) {
+void Debug_BackMemTest_Write2Eeeprom(void) {
+    if ((gContButtonPressed[0] & CONT_A) && (Eeprom_Write(&gContMesgQueue, &D_8004A610, 0, 0x200) != 0)) {
 
     }
 }
 
-void func_800203C0(void) {
-    if ((gContButtonPressed[0] & 0x8000) && (Eeprom_Read(&gContMesgQueue, D_80057240, 0, 0x200) != 0)) {
+void Debug_BackMemTest_Read_Eeprom(void) {
+    if ((gContButtonPressed[0] & CONT_A) && (Eeprom_Read(&gContMesgQueue, D_80057240, 0, 0x200) != 0)) {
     }
 }
 
-void func_80020420(void) {
+void Debug_BackupMemTest_Unk(void) {
     s32 sp1C;
 
-    if (gContDirPressed[0] & 0x800) {
-        if ((D_80057440--, D_80057440) < 0) {
-            D_80057440 = 0xF;
+    if (gContDirPressed[0] & CONT_UP) {
+        if (--gDebugBackMemTestItem < 0) {
+            gDebugBackMemTestItem = 15;
         }
-    } else if (gContDirPressed[0] & 0x400) {
-        if ((D_80057440++, D_80057440) >= 0x10) {
-            D_80057440 = 0;
+    } else if (gContDirPressed[0] & CONT_DOWN) {
+        if (++gDebugBackMemTestItem >= 16) {
+            gDebugBackMemTestItem = 0;
         }
     }
-    if (gContButtonPressed[0] & 0x8000) {
+    if (gContButtonPressed[0] & CONT_A) {
         sp1C = 0;
-        func_8001F728(D_80056E28, sp1C, D_80057040 + (D_80057440));
-        func_8001F328(D_80056E28, sp1C, D_80057440, D_80057040 + (D_80057440));
+        func_8001F728(D_80056E28, sp1C, D_80057040 + (gDebugBackMemTestItem));
+        func_8001F328(D_80056E28, sp1C, gDebugBackMemTestItem, D_80057040 + (gDebugBackMemTestItem));
     }
 }
 
-void func_80020550(void) {
-    s32 sp2C;
+UNUSED void Debug_BackupMemTest(void) {
+    s32 i;
     UNUSED s32 pad;
     s32 sp24;
 
-    if (gContButtonPressed[0] & 0x1000) {
+    if (gContButtonPressed[0] & CONT_START) {
         func_8001D2C0();
     }
     if (func_8001D1D4() == 1) {
@@ -455,85 +467,83 @@ void func_80020550(void) {
     if (func_8001D1D4() == 2) {
         D_80165264 = 1;
     }
-    if (gContButtonPressed[0] & 0x20) {
-        if (D_80057441 == 0) {
-            D_80057441 = 1;
-            D_80057440 = 0;
+    if (gContButtonPressed[0] & CONT_L) {
+        if (gDebugBackupMemTestMenu == MAIN_MENU) {
+            gDebugBackupMemTestMenu = UNK_MENU;
+            gDebugBackMemTestItem = 0;
             sp24 = 0;
             func_8001F174(D_80056E28, sp24);
-            for (sp2C = 0; sp2C < 16; sp2C++) {
-                func_8001F328(D_80056E28, sp24, sp2C, D_80057040 + (sp2C));
+            for (i = 0; i < 16; i++) {
+                func_8001F328(D_80056E28, sp24, i, D_80057040 + (i));
             }
         } else {
-            D_80057441 = 0;
-            D_80057440 = 0;
+            gDebugBackupMemTestMenu = MAIN_MENU;
+            gDebugBackMemTestItem = 0;
         }
     }
-    if (D_80057441 == 0) {
-        if (gContDirPressed[0] & 0x800) {
-            if ((D_80057440--, D_80057440) < 0) {
-                D_80057440 = 3;
+    if (gDebugBackupMemTestMenu == MAIN_MENU) {
+        if (gContDirPressed[0] & CONT_UP) {
+            if (--gDebugBackMemTestItem < 0) {
+                gDebugBackMemTestItem = 3;
             }
-        } else if (gContDirPressed[0] & 0x400) {
-         
-            if ((D_80057440++, D_80057440) >= 4) {
-                D_80057440 = 0;
+        } else if (gContDirPressed[0] & CONT_DOWN) {
+            if (++gDebugBackMemTestItem >= 4) {
+                gDebugBackMemTestItem = 0;
             }
         }
-        switch (D_80057440) {                       /* irregular */
-        case 0:
+        switch (gDebugBackMemTestItem) {                       /* irregular */
+        case PAK_DATA_WRITE:
             func_800200D8();
             break;
-        case 1:
+        case PAK_DATA_READ:
             func_8002021C();
             break;
-        case 2:
-            func_80020360();
+        case EEPROM_WRITE:
+            Debug_BackMemTest_Write2Eeeprom();
             break;
-        case 3:
-            func_800203C0();
+        case EEPROM_READ:
+            Debug_BackMemTest_Read_Eeprom();
             break;
         }
-    } else if (D_80057441 == 1) {
-        func_80020420();
+    } else if (gDebugBackupMemTestMenu == UNK_MENU) {
+        Debug_BackupMemTest_Unk();
     }
     func_8001F238(D_80056E28);
-    for (sp2C = 0; sp2C < 4; sp2C++) {
-        if ((u32)D_80056E98[sp2C].unk0 == 0) {
-            func_8001F38C(D_80056E28, sp2C);
+    for (i = 0; i < 4; i++) {
+        if ((u32)D_80056E98[i].unk0 == 0) {
+            func_8001F38C(D_80056E28, i);
         }
     }
     func_8001994C();
 }
 
-void func_80020844(void) {
-    u16 sp4E;
-    s32 sp48;
-    s32 sp44;
+UNUSED void Debug_BackupMemTest_Menu(void) {
+    u16 perspNorm;
+    s32 i;
+    s32 j;
 
     func_8001D4D0();
     func_8001D638(1, 64, 64, 0);
-    guPerspective(D_8016E104, &sp4E, 50.0f, 1.3333334f, 100.0f, 4000.0f, 1.0f);
-    gSPPerspNormalize(gMasterDisplayList++, sp4E);
+    guPerspective(D_8016E104, &perspNorm, 50.0f, 1.3333334f, 100.0f, 4000.0f, 1.0f);
+    gSPPerspNormalize(gMasterDisplayList++, perspNorm);
     guLookAt(&D_8016E104->unk00[2], gView.eye.x, gView.eye.y, gView.eye.z, gView.at.x, gView.at.y, gView.at.z, gView.up.x, gView.up.y, gView.up.z);
     gSPMatrix(gMasterDisplayList++, D_8016E104, G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_PROJECTION);
 
     D_8016E3A4 = 0;
     func_8005F96C(255, 255, 255);
-    switch (D_80057441) {                           /* irregular */
-    case 0:
+    switch (gDebugBackupMemTestMenu) {                           /* irregular */
+    case MAIN_MENU:
         sprintf((char*)gDebugTextBuf, "BACKUP MEMORY TEST");
         debug_print_xy(32, 32);
         sprintf((char*)gDebugTextBuf, "    P  I  A  N RW  F  f");
         debug_print_xy(48, 48);
-        for (sp48 = 0; sp48 < 4; sp48++) {
-
-            sprintf((char*)gDebugTextBuf, "%dp=%2d %2d %2d %2d %2d %2d %2d", sp48+1, 
-                D_80056E28[sp48].unk70, D_80056E28[sp48].unk74, 
-                D_80056E28[sp48].unk78, D_80056E28[sp48].unk7C, 
-                D_80056E28[sp48].unk80,  D_80056E28[sp48].unk6C, D_80056E28[sp48].unk68);
+        for (i = 0; i < 4; i++) {
+            sprintf((char*)gDebugTextBuf, "%dp=%2d %2d %2d %2d %2d %2d %2d", i+1, 
+                D_80056E28[i].unk70, D_80056E28[i].unk74, 
+                D_80056E28[i].unk78, D_80056E28[i].unk7C, 
+                D_80056E28[i].unk80,  D_80056E28[i].unk6C, D_80056E28[i].unk68);
             
-            debug_print_xy(48, (sp48 * 16) + 64);
+            debug_print_xy(48, (i * 16) + 64);
         }
         sprintf((char*)gDebugTextBuf, "PAK DATA WRITE");
         debug_print_xy(48, 144);
@@ -544,26 +554,25 @@ void func_80020844(void) {
         sprintf((char*)gDebugTextBuf, "EEPROM DATA READ");
         debug_print_xy(48, 192);
         sprintf((char*)gDebugTextBuf, "=");
-        debug_print_xy(32, (D_80057440 * 16) + 144);
+        debug_print_xy(32, (gDebugBackMemTestItem * 16) + 144);
         break;
-    case 1:
-        for (sp48 = 0; sp48 < 16; sp48++) {
-            for (sp44 = 0; sp44 < 16; sp44++) {
-                sprintf((char*)gDebugTextBuf, "%c", D_8005704E[sp48 << 5][sp44] + 32);
-                debug_print_xy((sp44 * 8) + 48, (sp48 * 16) + 16);
+    case UNK_MENU:
+        for (i = 0; i < 16; i++) {
+            for (j = 0; j < 16; j++) {
+                sprintf((char*)gDebugTextBuf, "%c", D_8005704E[i << 5][j] + 32);
+                debug_print_xy((j * 8) + 48, (i * 16) + 16);
             }
         } 
         sprintf((char*)gDebugTextBuf, "=");
-        debug_print_xy(32, (D_80057440 * 16) + 16);
+        debug_print_xy(32, (gDebugBackMemTestItem * 16) + 16);
         break;
     }
 }
 
-
-void func_80020C44(void) {
+void Debug_ShockTest(void) {
     UNUSED char pad[0x8];
 
-    if (gContButtonPressed[0] & 0x1000) {
+    if (gContButtonPressed[0] & CONT_START) {
         func_8001D2C0();
     }
     if (func_8001D1D4() == 1) {
@@ -572,11 +581,11 @@ void func_80020C44(void) {
     if (func_8001D1D4() == 2) {
         D_80165264 = 1;
     }
-    if (gContDirPressed[0] & 0x800) {
+    if (gContDirPressed[0] & CONT_UP) {
         if ((D_8004A63C--, D_8004A63C) < 0) {
             D_8004A63C = 2;
         }
-    } else if (gContDirPressed[0] & 0x400) {
+    } else if (gContDirPressed[0] & CONT_DOWN) {
 
         if ((D_8004A63C++, D_8004A63C) >= 3) {
             D_8004A63C = 0;
@@ -584,43 +593,43 @@ void func_80020C44(void) {
     }
     switch (D_8004A63C) {                           /* irregular */
     case 0:
-        if (gContDirPressed[0] & 0x200) {
+        if (gContDirPressed[0] & CONT_LEFT) {
             D_8004A630 -= 1;
-        } else if (gContDirPressed[0] & 0x100) {
+        } else if (gContDirPressed[0] & CONT_RIGHT) {
             D_8004A630 += 1;
         }
         break;
     case 1:
-        if (gContDirPressed[0] & 0x200) {
+        if (gContDirPressed[0] & CONT_LEFT) {
             D_8004A634 -= 1;
-        } else if (gContDirPressed[0] & 0x100) {
+        } else if (gContDirPressed[0] & CONT_RIGHT) {
             D_8004A634 += 1;
         }
         break;
     case 2:
-        if (gContDirPressed[0] & 0x200) {
+        if (gContDirPressed[0] & CONT_LEFT) {
             D_8004A638 -= 1;
-        } else if (gContDirPressed[0] & 0x100) {
+        } else if (gContDirPressed[0] & CONT_RIGHT) {
             D_8004A638 += 1;
         }
         break;
     }
-    if (gContButtonPressed[0] & 0x8000) {
+    if (gContButtonPressed[0] & CONT_A) {
         func_8001FB3C(D_8004A630, D_8004A634, D_8004A638);
     }
     func_8001FBAC();
     func_8001994C();
 }
 
-void* func_80020F18(void) {
-    u16 sp46;
+void Debug_ShockTest_Menu(void) {
+    u16 perspNorm;
     UNUSED char pad[0x8];
 
 
     func_8001D4D0();
     func_8001D638(1, 64, 64, 0);
-    guPerspective(D_8016E104, &sp46, 50.0f, 1.3333334f, 100.0f, 4000.0f, 1.0f);
-    gSPPerspNormalize(gMasterDisplayList++, sp46);
+    guPerspective(D_8016E104, &perspNorm, 50.0f, 1.3333334f, 100.0f, 4000.0f, 1.0f);
+    gSPPerspNormalize(gMasterDisplayList++, perspNorm);
 
     guLookAt(&D_8016E104->unk00[2], gView.eye.x, gView.eye.y, gView.eye.z, gView.at.x, gView.at.y, gView.at.z, gView.up.x, gView.up.y, gView.up.z);
     gSPMatrix(gMasterDisplayList++, D_8016E104, G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_PROJECTION);
@@ -641,8 +650,8 @@ void* func_80020F18(void) {
 
 s32 func_80021158(void) {
     func_8001ECB8();
-    D_8016526C = &func_80020F18;
-    D_80165274 = &func_80020C44;
+    D_8016526C = &Debug_ShockTest_Menu;
+    D_80165274 = &Debug_ShockTest;
     func_8001F9DC();
     func_8001E954((s32*)0x8024C000);
     func_8001E98C(0, &unk_bin_0_2_ROM_START, code_extra_0_ROM_START);
